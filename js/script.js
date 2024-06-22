d74b176f9e9281f13af4683a22eb469e8e56aa04
shivam chaudhary <118039146+helloshivamchaudhary@users.noreply.github.com>
2024-06-22 08:57:59 +0530

    Update script.js

--- js/script.js
+++ js/script.js
@@ -71,7 +71,7 @@ document.addEventListener('visibilitychange',
 
 // <!-- typed js effect starts -->
 var typed = new Typed(".typing-text", {
-    strings: ["frontend development", "backend development", "web designing", "android development", "Full Stack Web Development"],
+    strings: ["frontend development", "backend development", "web designing","Full Stack Development"],
     loop: true,
     typeSpeed: 60,
     backSpeed: 30,
